420eea910a12a997a4bf44344d04c8a1238314d0
amandeep-saluja <amandeepsaluja25@gmail.com>
2021-06-05 00:13:32 +0530

    added logs in account service

--- account-service/src/main/java/com/bank/account/controller/AccountController.java
+++ account-service/src/main/java/com/bank/account/controller/AccountController.java
@@ -6,6 +6,8 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -33,25 +35,33 @@ public class AccountController {
 	@Autowired
 	private AccountService service;
 	
+	private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
+	
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "{accId}")
 	public Account fetchAccount(@PathVariable(name = "accId") Integer accountId) {
+		logger.info("Account Fetch API triggered with account id: {} ",accountId);
 		return service.getAccountById(accountId);
 	}
 	
 	@GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Account> getAllAccounts() {
+		logger.info("Get all Accounts API triggered.");
 		return service.getAllAccounts();
 	}
 	
 	@PostMapping(value = "/add")
 	public Account createAccount(@Valid @RequestBody Account account) {
+		logger.info("New account created with account object: {}",account);
 		return service.createAccount(account);
 	}
 	
 	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Account updateAccount(@RequestParam("id") Integer id, @RequestParam("branch") String branchName,
 			@RequestParam("type") String type, @RequestParam("active") boolean active) {
-		return service.updateAccount(id, branchName, type, active);
+		Account account = service.updateAccount(id, branchName, type, active);
+		logger.info("Account update requested with id: {} , branch: {} , type: {} and active: {}", id, branchName, type, active);
+		logger.info("Updated account details: {} ",account);
+		return account;
 	}
 	
 	@DeleteMapping
@@ -66,6 +76,8 @@ public class AccountController {
 			@NotBlank(message = "Destination account number is required") @RequestParam("destination") String destination,
 			@NotNull(message = "Amount cannnot be empty") @RequestParam("amount") Float amount)
 			throws AccountNotFoundException {
+		logger.info("Transaction initiated in Account: {}");
+		logger.info("Sending {} INR from {} account to {} account", amount, source, destination);
 		return service.changeBalance(source, destination, amount);
 	}
 	
