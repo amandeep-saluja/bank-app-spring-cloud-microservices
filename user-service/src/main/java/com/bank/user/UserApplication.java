bba7b3cb17881ed119f533d3de52e73a23e65525
Amandeep Singh <amandeepsaluja25@gmail.com>
2021-06-12 00:29:49 +0530

    added zuul server

--- user-service/src/main/java/com/bank/user/UserApplication.java
+++ user-service/src/main/java/com/bank/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.bank.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -22,6 +23,7 @@ public class UserApplication {
 	}
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
