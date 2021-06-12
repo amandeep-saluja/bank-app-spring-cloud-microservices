942b79bc4db616c775d742e0dcb7805053a52d8a
Amandeep Singh <amandeepsaluja25@gmail.com>
2021-06-13 01:22:39 +0530

    added zipkin server project

---
+++ zipkin-server/src/main/java/com/bank/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,16 @@
+package com.bank.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+
+}
